5f58997b21beaf2fc37185599c01e235ba7d56c1
Rick M <kingthorin@users.noreply.github.com>
2025-02-19 10:50:55 -0500

    Update collect_vars.sh

--- scans/auth/collect_vars.sh
+++ scans/auth/collect_vars.sh
@@ -6,7 +6,10 @@ for d in ../../scans/auth/plans_and_scripts/**/; do
   then
     cat "$d"/vars.env | tee -a ../../scans/auth/all_vars.env > /dev/null
   fi
+  wc -l ../../scans/auth/all_vars.env
 done
+wc -l ../../scans/auth/all_vars.env
+
 echo $ALL_VARS | tee -a ../../scans/auth/all_vars.env > /dev/null
 
 wc -l ../../scans/auth/all_vars.env
