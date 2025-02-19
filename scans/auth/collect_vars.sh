4df6d20fb5715cfd4308e7880de1321654d96775
Rick M <kingthorin@users.noreply.github.com>
2025-02-19 10:28:37 -0500

    Update collect_vars.sh

--- scans/auth/collect_vars.sh
+++ scans/auth/collect_vars.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for d in ./scans/auth/plans_and_scripts/**/; do
+for d in ../../scans/auth/plans_and_scripts/**/; do
   echo $d
-  if ! [[ -s "$d/vars.env" ]];
+  if [[ -f "$d/vars.env" ]];
   then
-    # Here we'd get values from the secrets store
-    echo Secret
-  else
-    cat "$d"/vars.env | tee -a ./scans/auth/all_vars.env > /dev/null
+    cat "$d"/vars.env | tee -a ../../scans/auth/all_vars.env > /dev/null
   fi
 done
+echo ${{ secrets.ALL_VARS }} | tee -a ../../scans/auth/all_vars.env > /dev/null
+
+wc -l ../../scans/auth/all_vars.env
